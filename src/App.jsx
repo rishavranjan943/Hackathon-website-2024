8b83d5047c391e1e0dafb26701eaab8d314cfb0d
Rishav Ranjan <rishavranjan943@gmail.com>
2024-04-21 13:46:47 +0530

    Adding bottom Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './components/Footer'
+import Footer from './components/Footer'
+import Discord from './components/Discord'
 
 function App() {
 
@@ -10,6 +13,8 @@ function App() {
       <h1 className="text-3xl text-red-500 font-bold underline">
         statusCode1
       </h1>
+      <Discord/>
+      <Footer/>
     </>
   )
 }
