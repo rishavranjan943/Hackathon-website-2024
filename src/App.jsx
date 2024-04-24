fc1c114bf613202c5902abd366937fd52a2ec0e6
Bhaskar Metiya <bhaskarmetiya@gmail.com>
2024-04-25 02:48:20 +0530

    Remove undefined and unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Discord from './components/Discord/Discord'
@@ -9,9 +6,6 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl text-red-500 font-bold underline">
-        statusCode1
-      </h1>
       <Discord/>
       <Footer/>
     </>
