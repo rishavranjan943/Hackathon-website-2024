7048e71e57a258cf06e007bb545a88a69a10898e
Rishav Ranjan <rishavranjan943@gmail.com>
2024-06-12 13:12:53 +0530

    making discord section making background merged

--- src/components/Discord/Discord.jsx
+++ src/components/Discord/Discord.jsx
@@ -1,36 +1,20 @@
-import React from "react";
-import "./Discord.css";
-import discordSCImage from "../../assets/images/discord/text-fotor-20240610164442.png"; // Import the image
+import React from 'react';
+import Bgimage from '../../assets/images/discord/JOIN.png';
+import './Discord.css';
 
 function Discord() {
-  return (
-    <div>
-      <div className="hero">
-        <div className="background-container text-center">
-          <div className="image-container">
-            <img src={discordSCImage} alt="" className="darken-image" />
+    return (
+        <div className="section">
+            <div className="star-background"></div>
+            <img src={Bgimage} alt="" className='img-background' />
             <a
-              href="https://discord.gg/UHwPBzy7UF"
-              className="bottom-overlay"
-              target="_blank"
+                href="https://discord.gg/UHwPBzy7UF"
+                className="bottom-overlay"
+                target="_blank"
+                rel="noopener noreferrer"
             ></a>
-          </div>
         </div>
-        <div>
-          <div className="mx-auto px-4 py-8 text-center">
-            <p className="footercontent text-xl md:text-2xl  text-[#BEBEBE] text-center inline">
-              Join our Discord Community to stay updated on all the latest{" "}
-              <br className="hidden lg:inline" />
-              developments and announcements for our hackathon! Don&apos;t miss
-              out on <br className="hidden lg:inline" />
-              key updates and opportunities, join us today!
-            </p>
-          </div>
-        </div>
-      </div>
-      <div></div>
-    </div>
-  );
+    );
 }
 
 export default Discord;
