67153fe00abaff14ecb844a5e72f7ce8f7399315
Bhaskar Metiya <bhaskarmetiya@gmail.com>
2024-07-04 18:38:52 +0530

    update 2xl to 3xl in landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -63,7 +63,7 @@ function LandingPage() {
   return (
     <>
       <div
-        className=" h-full  2xl:h-[4000px] w-full flex flex-col text-center bg-no-repeat  bg-bg_color m-0"
+        className=" h-full  3xl:h-[4000px] w-full flex flex-col text-center bg-no-repeat  bg-bg_color m-0"
         style={{
           backgroundImage: `url(${BgLandingPage})`,
           backgroundPosition: `${50 + bgPosition.x}% ${20 + bgPosition.y}%`,
@@ -73,20 +73,20 @@ function LandingPage() {
         {/* Hero section */}
         <section
           id="hero"
-          className=" ml-5 mr-5 max-[540px]:mt-[60px] mt-[110px] 2xl:mt-[150px] "
+          className=" ml-5 mr-5 max-[540px]:mt-[60px] mt-[110px] 3xl:mt-[150px] "
         >
           <div>
             <h1
-              className="text-white min-[320px]:text-5xl md:text-7xl   xl:text-8xl  2xl:text-[12vh]  font-vt323"
+              className="text-white min-[320px]:text-5xl md:text-7xl   xl:text-8xl  3xl:text-[12vh]  font-vt323"
               style={{ textShadow: "2px 2px 0 rgb(236, 83, 176, 1)" }}
             >
               STATUS CODE <span className="text-red">1</span>
             </h1>
-            <h3 className="text-purple-900 font-vt323 min-[320px]:text-[20px] md:text-3xl xl:text-[40px] 2xl:m-11 2xl:p-10 2xl:text-[4vh] bg-gradient-to-r from-gradient_blue_purple-0 via-gradient_blue_purple-50 to-gradient_blue_purple-100  text-transparent bg-clip-text">
+            <h3 className="text-purple-900 font-vt323 min-[320px]:text-[20px] md:text-3xl xl:text-[40px] 3xl:m-11 3xl:p-10 3xl:text-[4vh] bg-gradient-to-r from-gradient_blue_purple-0 via-gradient_blue_purple-50 to-gradient_blue_purple-100  text-transparent bg-clip-text">
               Code With Purpose & Innovate, Collaborate, Dominate
             </h3>
           </div>
-          <div className="flex text-center justify-center mt-5 ml-5 mr-5 2xl:m-11">
+          <div className="flex text-center justify-center mt-5 ml-5 mr-5 3xl:m-11">
             <div
               className="bg-top bg-no-repeat flex items-end flex-col justify-center md:h-[80%] bg-cover "
               style={{
@@ -109,13 +109,13 @@ function LandingPage() {
                   <button  onClick={() =>
                     window.open("https://statuscode-1.devfolio.co/")
                   }
-                   className="text-purple font-vt323 text-2xl text-center justify-center  flex  mt-2 mb-2 gap-3 2xl:gap-11 2xl:text-7xl 2xl:m-5     ">
-                    <img className="2xl:w-[5vh]" src={DevfolioLogo} />
-                    <p className="2xl:mt-5">Apply with Devfolio </p>
+                   className="text-purple font-vt323 text-2xl text-center justify-center  flex  mt-2 mb-2 gap-3 3xl:gap-11 3xl:text-7xl 3xl:m-5     ">
+                    <img className="3xl:w-[5vh]" src={DevfolioLogo} />
+                    <p className="3xl:mt-5">Apply with Devfolio </p>
                   </button>
                 </div>
                 {/* </a> */}
-                <p className="max-[540px]:leading-normal md:leading-tight xl:leading-normal md:text-[22px] xl:text-[24px] 2xl:text-[3vh] 2xl:pl-[17vh] 2xl:pr-[17vh] 2xl:p-11 font-vt323 min-[320px]:text-[16px] md:text-2xl m-3 bg-gradient-to-b text-purple bg-clip-text">
+                <p className="max-[540px]:leading-normal md:leading-tight xl:leading-normal md:text-[22px] xl:text-[24px] 3xl:text-[3vh] 3xl:pl-[17vh] 3xl:pr-[17vh] 3xl:p-11 font-vt323 min-[320px]:text-[16px] md:text-2xl m-3 bg-gradient-to-b text-purple bg-clip-text">
                   Unleash your creativity and join Status Code 1, an official
                   MLH Member Event and the ultimate 36-hour hackathon
                   extravaganza! Compete across diverse categories, showcasing
@@ -124,7 +124,7 @@ function LandingPage() {
                   challenge yourself,
                 </p>
 
-                <button className="animate-bounce 2xl:w-[5%]  ">
+                <button className="animate-bounce 3xl:w-[5%]  ">
                   <a onClick={scrollToTimer}>
                     {" "}
                     <img src={ScrollButton} alt="Down" width="80% " />
